e81506e36a6b6f62e4b62a2d6c516daf364eebca
WKingDeveloper <wkingdeveloper@gmail.com>
2023-12-20 16:24:57 +0900

    Initial commit

---
+++ src/test/kotlin/com/wkingdeveloper/elastic/ElasticApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wkingdeveloper.elastic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ElasticApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
