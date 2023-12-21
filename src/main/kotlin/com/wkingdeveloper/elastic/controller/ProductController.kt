c5cd24a631b21d24b7e275e2ca1c6a10de819298
WKingDeveloper <wkingdeveloper@gmail.com>
2023-12-21 13:44:31 +0900

    feat-new : elastic bulk indexing 기능 추가

--- src/main/kotlin/com/wkingdeveloper/elastic/controller/ProductController.kt
+++ src/main/kotlin/com/wkingdeveloper/elastic/controller/ProductController.kt
@@ -2,13 +2,17 @@ package com.wkingdeveloper.elastic.controller
 
 import com.wkingdeveloper.elastic.domain.Product
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations
+import org.springframework.data.elasticsearch.core.IndexedObjectInformation
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates
+import org.springframework.data.elasticsearch.core.query.IndexQuery
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder
 import org.springframework.web.bind.annotation.*
 
 
 @RestController
 @RequestMapping("/")
 class ProductController (
-    private val elasticsearchOperations : ElasticsearchOperations
+    private val elasticsearchOperations : ElasticsearchOperations,
 ) {
     @PostMapping("/product")
     fun save(@RequestBody product: Product): Long {
@@ -16,9 +20,21 @@ class ProductController (
         return savedEntity.id
     }
 
-    @GetMapping("/product/{id}")
+    @PostMapping("/products")
+    fun saveAll(@RequestBody products: List<Product>) : MutableList<IndexedObjectInformation> {
+        val queryList: List<IndexQuery> = products.map { product ->
+                IndexQueryBuilder()
+                    .withId(product.id.toString())
+                    .withObject(product).build()
+            }
+            .toList()
+
+        return elasticsearchOperations.bulkIndex(queryList, IndexCoordinates.of("search-product"))
+    }
+
+
+    @GetMapping("/products/{id}")
     fun findById(@PathVariable("id") id: Long): Product? {
-        println("ProductController findById() id : $id")
         return elasticsearchOperations.get(id.toString(), Product::class.java)
     }
 }
